6583d056f86c5210cc329126b20ea871659fef3f
anj259 <92106605+anj259@users.noreply.github.com>
2025-12-23 00:14:31 +0530

    created middleware for auth token validation

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+// Middleware(method) to verify JWT token
+const authenticatejwtToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; 
+
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. Token missing." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(403).json({ message: "Invalid or expired token" });
+    }
+};
+
+module.exports = authenticatejwtToken;

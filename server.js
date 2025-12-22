1f1554eff2c0d9bfa564bd8eb703c2c1a027eee4
anj259 <92106605+anj259@users.noreply.github.com>
2025-12-23 01:08:33 +0530

    added post route in server file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config(); 
 const db = require("./config/db_connection");
 const user_route = require("./routes/user_route");
+const post_route= require("./routes/post_route")
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 db();
 
 app.use("/api/auth",user_route);
+app.use("/api/post",post_route);
+
+
 
 
 app.listen(PORT, () => {
